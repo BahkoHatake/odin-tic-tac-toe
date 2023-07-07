66bca7a93d8738f0156624adf78e334aef45579c
Branko Cvetanović <cbranko01@gmail.com>
2023-07-08 01:06:00 +0200

    add gameController objecrt with functions for placeing markers and switching current player

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const fields=document.querySelectorAll(".board>div")
 const gameBoard =(()=>{
-    const gameboard=["X","O","X","O","X","O","X","O","X"];
+    const board=["",null,null,null,null,null,null,null,null];
     return{
-        gameboard
+        board
     }
 })();
 const Player=(name,marker)=>{
@@ -10,11 +10,32 @@ const Player=(name,marker)=>{
 }
 const player1=Player("Branko","O");
 const player2=Player("Djole","X");
+ 
+const gameController=(()=>{
+    let currentPlayer=player1;
+    const switchPlayer=()=>{
+        if (currentPlayer==player1){currentPlayer=player2}
+        else if (currentPlayer==player2){currentPlayer=player1}
+    }
+    const oneTurn=()=>{
+        fields.forEach(field=>{field.addEventListener("click",(e)=>{
+            if (field.innerHTML!=0){
+                return
+            }
+            gameBoard.board.splice(e.target.id,1,currentPlayer.marker)
+            fillBoard()
+            switchPlayer()
+        })
+    })}
+    return{
+        oneTurn,
+        currentPlayer,
+    }
+})();
 
-gameController.click()
+gameController.oneTurn()
 const fillBoard=()=>{
-    gameBoard.gameboard.forEach((element,index)=>{
-        console.log(index)
+    gameBoard.board.forEach((element,index)=>{
         fields[index].innerHTML=element
     })
 };
